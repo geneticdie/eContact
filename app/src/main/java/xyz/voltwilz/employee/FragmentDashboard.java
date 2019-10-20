54afcaa30b59d9fe14c783e961706f2fd967a53f
geneticdie <wilyanto24@gmail.com>
2019-10-20 13:25:11 +0700

    Updating dashboard

--- app/src/main/java/xyz/voltwilz/employee/FragmentDashboard.java
+++ app/src/main/java/xyz/voltwilz/employee/FragmentDashboard.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 public class FragmentDashboard extends Fragment {
 
     Button btnTransaction, btnBudgeting;
+    RelativeLayout layoutBudget, layoutTransaction;
     View v;
 
     @Nullable
@@ -21,8 +23,24 @@ public class FragmentDashboard extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        btnBudgeting = v.findViewById(R.id.home_btnBudgeting);
-        btnTransaction = v.findViewById(R.id.home_btnTransaction);
+        layoutBudget  = v.findViewById(R.id.dashboard_layoutBudget);
+        layoutTransaction = v.findViewById(R.id.dashboard_layoutTransaction);
+        btnBudgeting = v.findViewById(R.id.dashboard_btnBudgeting);
+        btnTransaction = v.findViewById(R.id.dashboard_btnTransaction);
+
+        layoutBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ActivityBudgeting.class));
+            }
+        });
+
+        layoutTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ActivityTransaction.class));
+            }
+        });
 
         btnBudgeting.setOnClickListener(new View.OnClickListener() {
             @Override
